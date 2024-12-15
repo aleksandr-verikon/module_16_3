727ee88d5980b09c50845c5a0794eb5f775fdb58
aleksandr-verikon <sashava07@gmail.com>
2024-12-15 16:21:03 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 users = {'1': 'Имя: Example, возраст: 18'}
@@ -9,17 +8,21 @@ async def func_get() -> dict:
     return users
 
 @app.post("/user/{username}/{age}")
-async def func_post(user = str, username = str, age = str) -> dict:
-    user_id = str(int(max(users, key=int)) + 1)
+async def func_post(username: str, age: int) -> str:
+    user_id = str(int(max(users.keys())) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"User {user_id} is registered"
+    return f"User  {user_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def func_put(user = str, user_id = str, username = str, age = str) -> dict:
+async def func_put(user_id: str, username: str, age: int) -> str:
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User  not found")
     users[user_id] = f"Имя: {username}, возраст: {age}"
-    return "The user <user_id> is updated"
+    return f"The user {user_id} is updated"
 
-app.delete("/user/{user_id}")
-async def func_delete(user_id = str) -> str:
+@app.delete("/user/{user_id}")
+async def func_delete(user_id: str) -> str:
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User  not found")
     users.pop(user_id)
-    pass
+    return f"User  {user_id} has been deleted"
